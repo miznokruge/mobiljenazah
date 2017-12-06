444e136475ebf74967e3659af2885d10a4f979e8
ad3n <surya.kejawen@gmail.com>
2017-12-06 17:00:55 +0700

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MobilJenazah\Providers;
+
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
+    }
+}
