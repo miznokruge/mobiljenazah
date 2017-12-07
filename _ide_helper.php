5ac9e306326d60b59c5266b54cfd7a44eeae2ac1
ad3n <surya.kejawen@gmail.com>
2017-12-07 10:24:24 +0700

    improve autoload

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel Lumen (5.5.2) (Laravel Components 5.5.*) on 2017-12-06.
+ * Generated for Laravel Lumen (5.5.2) (Laravel Components 5.5.*) on 2017-12-07.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
